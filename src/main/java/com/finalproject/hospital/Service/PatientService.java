873cb8d4d2ed42678c3651a199ec719a4e9a3b3c
Harshavardhan <psharshavardhan69@gmail.com>
2025-04-08 06:31:24 +0530

    first

--- src/main/java/com/finalproject/hospital/Service/PatientService.java
+++ src/main/java/com/finalproject/hospital/Service/PatientService.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface PatientService {
     Patient registerPatient(Patient patient);
+
     Patient getPatientById(Long id);
+
     List<Patient> getAllPatients();
+
     void deletePatient(Long id);
 }
